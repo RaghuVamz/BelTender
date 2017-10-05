1b9442ffde6be97332e89f2aa17137c41b5aaa70
RaghuVamz <raghuvamsi1992@gmail.com>
2017-10-06 01:38:52 +0530

    fixing branch

--- art/scripts/env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/msp/sampleconfig
--- art/scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export PEER=$GOPATH/src/github.com/hyperledger/build/bin/peer
